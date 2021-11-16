be3f2c63a523aa9ffe2db197027e55d15b8d0d4c
Arpit67 <91089676+Arpit67@users.noreply.github.com>
2021-11-15 19:02:52 -0800

    my first test program

---
+++ firstprogram.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+int fog=7;
+void sum()
+{
+
+cout<<fog;
+}
+
+void gum()
+{
+    bool x= true;
+    cout<<x;
+}
+
+int main()
+{
+int a =8,b=9;
+int fog=3;
+sum();
+gum();
+
+    cout<<a<<b;
+cout<<fog;
+    return 0;
+
+}
